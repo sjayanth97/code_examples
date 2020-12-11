b12b16d1aea3c7e1877607bfbba692d9c938dc61
Anirudh <saianirudhjayanthi@outlook.com>
2020-12-10 21:25:00 -0600

    cpp file

---
+++ hello_world.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+
+int main()
+{
+    std::vector<std::string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    for (const std::string& word : msg)
+    {
+        std::cout << word << " "<<std::endl;
+    }
+    std::cout << std::endl;
+}
+    
